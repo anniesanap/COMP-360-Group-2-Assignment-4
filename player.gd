1dec19fb075d0e9ec3774790b75530ee39951b05
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-11-30 01:22:16 -0800

    Clean up character.gd

--- player.gd
+++ player.gd
@@ -9,28 +9,25 @@ func _ready() -> void:
 	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
+	# Rotate camera
 	if event is InputEventMouseMotion:
 		camera.rotation.x -= event.screen_relative.y * 0.002
 		camera.rotation.y -= event.screen_relative.x * 0.002
+	# Allow mouse to leave window
 	elif event.is_action_pressed("escape"):
 		DisplayServer.mouse_set_mode(2 * 1 - (int(DisplayServer.mouse_get_mode())))
+	# Handle jump.
+	elif Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Handle movement keys
+	else:
+		var movement = Input.get_vector("move_left", "move_right", "move_forward", "move_back").rotated(-camera.rotation.y)
+		velocity.x = -movement.y * SPEED
+		velocity.z = movement.x * SPEED
