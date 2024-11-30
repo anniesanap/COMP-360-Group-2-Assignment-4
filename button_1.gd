75acde684a2b1fa854d71216f72e73f4e855c2e6
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-11-30 01:48:55 -0800

    Change button colors on press

--- button_1.gd
+++ button_1.gd
@@ -1,6 +1,12 @@
 extends StaticBody3D
 
-@onready var arm = $".."/arm
+@onready var button_2_material: StandardMaterial3D = $".."/button2/buttonMesh.mesh.material
+@onready var button_3_material: StandardMaterial3D = $".."/button3/buttonMesh.mesh.material
+@onready var arm: Node3D = $".."/arm
+@onready var material: StandardMaterial3D = $buttonMesh.mesh.material
 
 func press():
+	button_2_material.albedo_color = Color.RED
+	button_3_material.albedo_color = Color.RED
+	material.albedo_color = Color.GREEN
 	create_tween().tween_property(arm, "rotation:y", PI / 4, 1).set_delay(0.5).set_trans(Tween.TRANS_SPRING)
