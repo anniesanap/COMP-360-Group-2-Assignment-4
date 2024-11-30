604254262609d9486493ba046993bcf603e951a8
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-11-30 02:10:36 -0800

    Change button back to red after a bit

--- button_2.gd
+++ button_2.gd
@@ -1,14 +1,11 @@
 extends StaticBody3D
 
-@onready var button_1_material: StandardMaterial3D = $".."/button1/buttonMesh.mesh.material
-@onready var button_3_material: StandardMaterial3D = $".."/button3/buttonMesh.mesh.material
 @onready var arm: Node3D = $".."/arm
 @onready var material: StandardMaterial3D = $buttonMesh.mesh.material
 
 func press():
-	create_tween().tween_property(button_1_material, "albedo_color", Color.RED, 0.2)
-	create_tween().tween_property(button_3_material, "albedo_color", Color.RED, 0.2)
 	create_tween().tween_property(material, "albedo_color", Color.GREEN, 0.2)
 	var rotate_tween = create_tween()
 	rotate_tween.tween_property(arm, "rotation:y", PI / 2, 1).set_delay(0.5).set_trans(Tween.TRANS_SPRING)
+	rotate_tween.tween_property(material, "albedo_color", Color.RED, 0.2)
 	rotate_tween.finished.connect(arm.arm_grab)
