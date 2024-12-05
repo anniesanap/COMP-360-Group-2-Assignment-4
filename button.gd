20688fbda587bdb38f620100f913ca7012b4ad75
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-05 14:52:16 -0800

    Move all buttons to single script and expose rotation variable in editor

--- button.gd
+++ button.gd
@@ -1,11 +1,13 @@
 extends StaticBody3D
 
+@export var arm_rotation: float = PI / 2
+
 @onready var arm: Node3D = $".."/arm
 @onready var material: StandardMaterial3D = $buttonMesh.mesh.material
 
 func press():
 	create_tween().tween_property(material, "albedo_color", Color.GREEN, 0.2)
 	var rotate_tween = create_tween()
-	rotate_tween.tween_property(arm, "rotation:y", 3 * PI / 4, 1).set_delay(0.5).set_trans(Tween.TRANS_SPRING)
+	rotate_tween.tween_property(arm, "rotation:y", arm_rotation, 1).set_delay(0.5).set_trans(Tween.TRANS_SPRING)
 	rotate_tween.tween_property(material, "albedo_color", Color.RED, 0.2)
 	rotate_tween.finished.connect(arm.arm_grab)
