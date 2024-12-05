3af82b1a771a376ed693ad5d801f34427ecd05a8
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-05 15:07:53 -0800

    Cannot press button if arm is not idle

--- arm.gd
+++ arm.gd
@@ -2,4 +2,6 @@ extends Node3D
 
 @onready var animation_player = $AnimationPlayer
 
-var arm_grab: Callable = func(): animation_player.play("grab"); animation_player.queue("pickup")
+var arm_grab: Callable = func(): animation_player.play("grab"); rotating = false; animation_player.queue("pickup"); animation_player.queue("idle")
+
+var rotating: bool = false
--- button.gd
+++ button.gd
@@ -6,8 +6,10 @@ extends StaticBody3D
 @onready var material: StandardMaterial3D = $buttonMesh.mesh.material
 
 func press():
-	create_tween().tween_property(material, "albedo_color", Color.GREEN, 0.2)
-	var rotate_tween = create_tween()
-	rotate_tween.tween_property(arm, "rotation:y", arm_rotation, abs(arm.rotation.y - arm_rotation) / (PI / 4)).set_delay(0.5).set_trans(Tween.TRANS_SPRING)
-	rotate_tween.tween_property(material, "albedo_color", Color.RED, 0.2)
-	rotate_tween.finished.connect(arm.arm_grab)
+	if not arm.rotating and arm.animation_player.current_animation == "idle":
+		arm.rotating = true
+		create_tween().tween_property(material, "albedo_color", Color.GREEN, 0.2)
+		var rotate_tween = create_tween()
+		rotate_tween.tween_property(arm, "rotation:y", arm_rotation, abs(arm.rotation.y - arm_rotation) / (PI / 4)).set_delay(0.5).set_trans(Tween.TRANS_SPRING)
+		rotate_tween.tween_property(material, "albedo_color", Color.RED, 0.2)
+		rotate_tween.finished.connect(arm.arm_grab)
