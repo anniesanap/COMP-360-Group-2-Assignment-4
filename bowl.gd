1e8cd794d20ee3500ff8639c91190c1b17b52605
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-05 15:10:29 -0800

    Remove individual bowl scripts in favour of a single script

--- bowl.gd
+++ bowl.gd
@@ -3,9 +3,8 @@ var initialImpulse = Vector3(0,5,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	apply_impulse(initialImpulse)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
