d0721288d098708604654cc7c07696aac9516a3c
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-05 15:22:36 -0800

    Play drop animation after pickup

--- arm.gd
+++ arm.gd
@@ -2,6 +2,17 @@ extends Node3D
 
 @onready var animation_player = $AnimationPlayer
 
-var arm_grab: Callable = func(): animation_player.play("grab"); rotating = false; animation_player.queue("pickup"); animation_player.queue("idle")
+func arm_grab():
+	animation_player.play("grab")
+	rotating = false; animation_player.queue("pickup")
+	animation_player.queue("letgo")
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(
+		func(animation_name: String) -> void:
+			if animation_name == "letgo":
+				animation_player.play_backwards("letgo")
+				animation_player.queue("idle")
+	)
 
 var rotating: bool = false
