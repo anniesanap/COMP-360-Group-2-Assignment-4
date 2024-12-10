25346ae9e10799c8e12fa03d6a5f459c4f7d5562
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-09 19:23:48 -0800

    Actually do greyboxing for coin and orange

--- root.gd
+++ root.gd
@@ -7,7 +7,12 @@ var rng = RandomNumberGenerator.new()
 func _ready() -> void:
 	var coin_bowl = rng.randi_range(0, len(bowls) - 1)
 	for bowl in bowls:
-		if bowl == coin_bowl:
+		var object: RigidBody3D
+		if bowl == bowls[coin_bowl]:
 			bowl.add_child(load("res://coin_explosion.tscn").instantiate())
+			object = load("res://coin.tscn").instantiate()
 		else:
-			pass
+			object = load("res://orange.tscn").instantiate()
+		object.transform.origin = bowl.global_position
+		object.transform.origin.y = 1.0
+		add_child(object)
