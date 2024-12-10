4eec339ad021e565f34b1ba3f9ab4279fbad597b
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-10 02:23:10 -0800

    Clean up bowl script

--- bowl.gd
+++ bowl.gd
@@ -4,14 +4,12 @@ var object: RigidBody3D = null
 @onready var coin_explosion: GPUParticles3D = $"coin explosion"
 var coin: bool = false
 
-@onready var root = $/root/root
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_impulse(initialImpulse)
 
 func spawn_item() -> void:
 	if object.get_parent() == null:
-		root.add_child(object)
+		get_parent_node_3d().add_child(object)
 	if coin:
 		coin_explosion.emitting = true
