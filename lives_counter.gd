b1028655b013beb0f559277507eb06ce6f0d015d
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-11 22:21:23 -0800

    Minor code cleanup

--- lives_counter.gd
+++ lives_counter.gd
@@ -8,13 +8,6 @@ extends StaticBody3D
 var lives: float = 2.0
 var max_lives: float = 2.0
 
-func _ready() -> void:
-	bar_shader.set_shader_parameter("lives", lives)
-	bar_shader.set_shader_parameter("max_lives", max_lives)
-	cap_shader.set_shader_parameter("lives", lives)
-	cap_shader.set_shader_parameter("max_lives", max_lives)
-	cap.position.y = life_meter.position.y + ((lives / max_lives) * life_meter.mesh.height) - (life_meter.mesh.height / 2)
-
 func set_lives(new_lives: float, new_max_lives: float = max_lives) -> void:
 	create_tween().tween_method(func(new_value) -> void: bar_shader.set_shader_parameter("lives", new_value), lives, new_lives, 3.0).set_trans(Tween.TRANS_CUBIC)
 	create_tween().tween_method(func(new_value) -> void: cap_shader.set_shader_parameter("lives", new_value), lives, new_lives, 3.0).set_trans(Tween.TRANS_CUBIC)
