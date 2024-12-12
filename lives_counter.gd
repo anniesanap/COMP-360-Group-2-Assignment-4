03fbde0d376a1ba8f3934f0ee21d2c15115438b5
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-11 19:58:35 -0800

    Fix crash

--- lives_counter.gd
+++ lives_counter.gd
@@ -1,7 +1,10 @@
 extends StaticBody3D
 
-@onready var life_meter: StaticBody3D = $lifeMeter
+@onready var life_meter: MeshInstance3D = $lifeMeter
+@onready var bar_shader: ShaderMaterial = $lifeMeter.mesh.material
+@onready var cap_shader: ShaderMaterial = $lifeMeter/lifeMeterCap.mesh.material
 var lives: float = 2.0
+var max_lives: float = 2.0
 
 func _ready() -> void:
 	pass
