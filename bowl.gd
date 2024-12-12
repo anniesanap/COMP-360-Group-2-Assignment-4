2694af7508db98dd594b7a2ebce5f3200db81c07
Grant <perfectwing1999@gmail.com>
2024-12-11 16:10:46 -0800

    Store initial position and added function to reset to it.

--- bowl.gd
+++ bowl.gd
@@ -4,10 +4,14 @@ var object: RigidBody3D = null
 @onready var coin_explosion: GPUParticles3D = $"coin explosion"
 var coin: bool = false
 var random = RandomNumberGenerator.new()
+@onready var initialPosition: Vector3 = global_transform.origin
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_impulse(initialImpulse)
+
+func _get_position() -> Vector3:
+	return Vector3(self.x,self.y,self.z)
 
 func spawn_item() -> void:
 	if object.get_parent() == null:
@@ -16,5 +20,7 @@ func spawn_item() -> void:
 		coin_explosion.emitting = true
 
 func _throw() -> void:
-	if(true):
-		apply_impulse(Vector3(-10,0,random.randi_range(-10,10)))
+	apply_impulse(Vector3(-10,0,random.randi_range(-10,10)))
+
+func _reset_position() -> void:
+	self.global_transform.origin = initialPosition
