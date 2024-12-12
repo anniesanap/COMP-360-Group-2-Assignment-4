87fc302e70c4cc73229349fec15df87bbb181e23
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2024-12-12 14:06:34 -0800

    Fix bowl and item logic

--- bowl.gd
+++ bowl.gd
@@ -5,6 +5,7 @@ var object: RigidBody3D = null
 @onready var wrong_bowl: GPUParticles3D = $"thumbs_down"
 var coin: bool = false
 @onready var initial_transform: Transform3D = global_transform
+@onready var player: CharacterBody3D = $".."/player
 
 func _ready() -> void:
 	apply_impulse(initial_impulse)
@@ -18,13 +19,17 @@ func take_item() -> void:
 		object.get_parent().remove_child(object)
 
 func reset() -> void:
-	freeze = true
+	add_collision_exception_with(player)
 	var reset_tween: Tween = create_tween()
 	reset_tween.tween_property(self, "global_transform", initial_transform, 3.0)
 	reset_tween.tween_callback(
 		func() -> void:
+			object.transform.origin = global_position
+			object.rotation = Vector3.ZERO
+			object.angular_velocity = Vector3.ZERO
+			object.linear_velocity = Vector3.ZERO
 			angular_velocity = Vector3.ZERO
 			linear_velocity = Vector3.ZERO
-			freeze = false
+			remove_collision_exception_with(player)
 			take_item()
 	)
